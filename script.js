2090ac86d0a40184e262874f098cbe2a69685808
Katie Fritz <katie@katiemfritz.com>
2014-12-06 15:05:48 -0500

    things work now, still want to do more styling

--- script.js
+++ script.js
@@ -13,10 +13,15 @@ function makeBlue()
        document.getElementById("caturday-body").className = "surprise";
     }
 
+ function reset()
+ 	{
+ 		document.getElementById("caturday-body").className = "";
+ 	}
+
  window.onload = function()
     {
         document.getElementById("greenbutton").addEventListener( 'click' , makeGreen );
         document.getElementById("blue").addEventListener( 'click' , makeBlue );
         document.getElementById("surprise").addEventListener( 'click' , makeSurprise );
-    }
-
+        document.getElementById("reset").addEventListener( 'click' , reset );
+    };
